481cd271574d4244eca601ba413df342536b8a8e
Albarracin-sg <camilinplin2024@gmail.com>
2025-03-03 15:20:39 -0500

    modificacion de la pagina formop ahora mainOp

---
+++ frontend/src/components/operador/mainOperador.jsx
@@ -0,0 +1,8 @@
+    import { useState } from "react";
+    import ipsLogo from "../../assets/ips.png";
+
+    const mainOperador = () => {
+        
+    };
+    
+    export default mainOperador;
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Form from "../components/qr_option/form";
 import Ticket from "../components/qr_option/ticket";
-import Formop from "../components/operador/form_op";
+import MainOperador from "../components/operador/mainOperador";
 import Screen from "../components/mainscreen/screen";
 import QR from "../components/qr_option/qr";
 
@@ -10,7 +10,7 @@ function AppRoutes() {
         <Routes>
             <Route path="/" element={<Form />} />
             <Route path="/ticket" element={<Ticket />} />
-            <Route path="/formop" element={<Formop />} />
+            <Route path="/mainOp" element={<MainOperador />} />
             <Route path="/screen" element={<Screen />} />
             <Route path="/qr" element={<QR />} />
         </Routes>
