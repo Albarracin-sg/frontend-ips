caa5c7540f7e0c93cb795c644ae8a22da4aa1988
Albarracin-sg <camilinplin2024@gmail.com>
2025-03-06 14:34:02 -0500

    importacion del componete ticket

--- frontend/src/components/operador/TurnosOperador.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react' // Importa la librería de React
-import ipsLogo from '../../assets/ips.png' // Importa la imagen del logo de IPS'
-// Define el componente funcional BarraLateral
-const TurnosOPERADOR = () => {
-	return 
-}
-
-// Exporta el componente para que pueda ser utilizado en otras partes de la aplicación
-export default TurnosOPERADOR
