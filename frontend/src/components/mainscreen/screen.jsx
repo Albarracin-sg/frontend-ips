ee396219e5e47a1656a410b70369bc29d19b98d3
nico4348 <nico4348@gmail.com>
2025-03-06 15:02:42 -0500

    ajustes de screen

--- frontend/src/components/mainscreen/screen.jsx
+++ frontend/src/components/mainscreen/screen.jsx
@@ -1,109 +1,108 @@
-import { useState, useEffect } from "react";
-import ipsLogo from "../../assets/ipsBlack.png";
+import { useState, useEffect } from 'react'
+import ipsLogo from '../../assets/ipsBlack.png'
 
 /**
  * Componente funcional Screen
  * Este componente representa una pantalla con una lista de turnos y la información del turno actual.
  */
 const Screen = () => {
-  const [currentTime, setCurrentTime] = useState(new Date());
+	const [currentTime, setCurrentTime] = useState(new Date())
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000); // Actualiza cada segundo
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentTime(new Date())
+		}, 1000) // Actualiza cada segundo
 
-    return () => clearInterval(interval); // Limpia el intervalo al desmontar
-  }, []);
+		return () => clearInterval(interval) // Limpia el intervalo al desmontar
+	}, [])
 
-  // Formato de fecha y hora
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  const formattedDate = currentTime.toLocaleDateString("es-ES", options);
-  const formattedTime = currentTime.toLocaleTimeString("es-ES");
+	// Formato de fecha y hora
+	const options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+	}
+	const formattedDate = currentTime.toLocaleDateString('es-ES', options)
+	const formattedTime = currentTime.toLocaleTimeString('es-ES')
 
-  return (
-    <div className="min-h-screen w-screen bg-[#d9d9d9] border-[12px] border-[#3c3c3c] p-4 box-border">
-      <div className="flex flex- flex-row h-full ">
-        {/* Sección izquierda - Cola de turnos */}
-        <div className="flex-1 flex flex-col gap-4 ">
-          {/* Encabezado de la cola de turnos */}
-          <div className="relative w-full h-[90px] bg-none border-[2px] rounded-[20px] flex items-center justify-between px-6">
-            <h2 className="font-bold text-lg sm:text-xl whitespace-pre-line">
-              MODULO /{"\n"} CONSULTA
-            </h2>
-            <div className="flex gap-50">
-              <h2 className="font-medium text-xl">NOMBRE</h2>
-              <h2 className="font-medium text-xl">TURNO</h2>
-            </div>
-          </div>
+	return (
+		<div className="relative h-screen w-screen bg-[#d9d9d9] border-[12px] border-[#3c3c3c] box-border">
+			<div className="relative  flex- flex-row h-full ">
+				{/* Sección izquierda - Cola de turnos */}
+				{/* Logo - Posicionado en la esquina superior derecha */}
+				<div className="absolute top-[20px] right-[20px]">
+					<img src={ipsLogo} alt="IPS Logo" className="w-[120px]" />
+				</div>
 
-          {/* Elementos de la cola - Generados dinámicamente con map */}
-          {[1, 2, 3, 4, 5].map((index) => (
-            <div
-              key={index}
-              className={`relative w-full h-[90px] ${
-                index === 1 ? "bg-[#42A5F5]" : "bg-[#90CAF9]"
-              } rounded-[20px] flex items-center justify-between px-6`}
-            >
-              <div className="flex items-center gap-25">
-                <p className="font-bold text-xl">{index}</p>
-                <span className="text-5xl">|</span>
-                <p className="font-bold text-xl w-[150px] text-center">
-                  {
-                    [
-                      "Javier Gomez",
-                      "Camilo Albarracin",
-                      "Alex Quiroz",
-                      "Esteban Molina",
-                      "Santiago Salazar",
-                    ][index - 1]
-                  }
-                </p>
-              </div>
-              <div className="flex items-center gap-25">
-                <p className="text-4xl font-bold">-</p>
-                <p className="font-bold text-xl">D290</p>
-              </div>
-            </div>
-          ))}
-        </div>
+				{/* Sección superior - Turno actual */}
+				<div className="flex-1 relative h-44/100  flex justify-end mb-[40px] ">
+					<div className=" bg-[#d9d9d9] border-[18px] border-[#3c3c3c] w-2/6 h-full flex mr-[150px] mt-[12px] flex-col items-center justify-center">
+						{/* Decoración negra */}
+						<div className="absolute w-[192px] h-[60px] top-[-10px] bg-[#1E1E1E] rounded-[10px] " />
 
-        {/* Logo - Posicionado en la esquina superior derecha */}
-        <div className="absolute top-[20px] right-[20px]">
-          <img src={ipsLogo} alt="IPS Logo" className="w-[120px]" />
-        </div>
+						<h2 className="text-3xl font-bold text-center tracking-[3px] uppercase mb-8">
+							Turno actual
+						</h2>
+						<h2 className="text-3xl font-bold text-center uppercase mb-8">
+							Javier Alexander Gomez
+						</h2>
+						<h2 className="text-5xl font-bold text-center text-[#9B0000] uppercase">
+							TURNO
+						</h2>
 
-        {/* Sección derecha - Turno actual */}
-        <div className="flex-1 relative h-[540px] mt-[50px]  flex justify-center">
-          <div className="bg-[#d9d9d9] border-[18px] border-[#3c3c3c] h-[500px] w-[600px] rounded-[0] flex flex-col items-center justify-center">
-            {/* Decoración negra */}
-            <div className="relative w-[192px] h-[60px] top-[-110px] bg-[#1E1E1E] rounded-[10px]" />
+						{/* Fecha y Hora */}
+						<div className="relative bottom-[-10px] left-0 w-full flex flex-col items-center justify-center gap-4">
+							<h2 className="font-bold text-xl uppercase">{formattedDate}</h2>
+							<h2 className="font-bold text-xl">{formattedTime}</h2>
+						</div>
+					</div>
+				</div>
+				<div className="relative  flex flex-col gap-4 h-1/2">
+					{/* Encabezado de la cola de turnos */}
+					<div className="relative w-full h-[90px] bg-none border-[2px] rounded-[20px] flex items-center justify-between px-6">
+						<h2 className="font-bold text-xl whitespace-pre-line">
+							MODULO /{'\n'} CONSULTA
+						</h2>
+						<div className="flex gap-200">
+							<h2 className="font-medium text-xl">NOMBRE</h2>
+							<h2 className="font-medium text-xl">TURNO</h2>
+						</div>
+					</div>
 
-            <h2 className="text-3xl sm:text-4xl font-bold text-center tracking-[3px] uppercase mb-8">
-              Turno actual
-            </h2>
-            <h2 className="text-3xl sm:text-4xl font-bold text-center uppercase mb-8">
-              Javier Alexander Gomez
-            </h2>
-            <h2 className="text-5xl sm:text-6xl font-bold text-center text-[#9B0000] uppercase">
-              TURNO
-            </h2>
+					{/* Elementos de la cola - Generados dinámicamente con map */}
+					{[1, 2, 3, 4, 5].map((index) => (
+						<div
+							key={index}
+							className={`relative w-full h-[90px] ${
+								index === 1 ? 'bg-[#42A5F5]' : 'bg-[#90CAF9]'
+							} rounded-[20px] flex items-center justify-between px-6`}
+						>
+							<div className="flex items-center gap-25">
+								<p className="font-bold text-xl">{index}</p>
+								<span className="text-5xl">|</span>
+								<p className="font-bold text-xl text-center justify-center">
+									{
+										[
+											'Javier Gomez',
+											'Camilo Albarracin',
+											'Alex Quiroz',
+											'Esteban Molina',
+											'Santiago Salazar',
+										][index - 1]
+									}
+								</p>
+							</div>
+							<div className="flex items-center gap-50">
+								<p className="text-4xl font-bold">-</p>
+								<p className="font-bold text-xl">D290</p>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	)
+}
 
-            {/* Fecha y Hora */}
-            <div className="relative bottom-[-160px] left-0 w-full flex flex-col items-center justify-center gap-4">
-              <h2 className="font-bold text-xl uppercase">{formattedDate}</h2>
-              <h2 className="font-bold text-xl">{formattedTime}</h2>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Screen;
+export default Screen
--- frontend/src/components/operador/componets_op/barraLateral.jsx
+++ frontend/src/components/operador/componets_op/barraLateral.jsx
@@ -1,46 +1,46 @@
-import React from "react"; // Importa la librería de React
-import ipsLogo from "../../../assets/ips.png"; // Importa la imagen del logo de IPS
+import React from 'react' // Importa la librería de React
+import ipsLogo from '../../../assets/ips.png' // Importa la imagen del logo de IPS
 // Define el componente funcional BarraLateral
 const BarraLateral = () => {
-  return (
-    // Contenedor principal que usa flexbox para el diseño
-    <div className="flex">
-      {/* Barra lateral */}
-      <aside className="w-64 h-screen bg-gray-800 text-white flex flex-col">
-        {/* Contenedor del logo */}
-        <div className="p-4">
-          <img src={ipsLogo} alt="IPS Logo" className="w-32 h-15 mx-auto" />
-        </div>
+	return (
+		// Contenedor principal que usa flexbox para el diseño
+		<div className="flex">
+			{/* Barra lateral */}
+			<aside className="w-64 h-screen bg-gray-800 text-white flex flex-col">
+				{/* Contenedor del logo */}
+				<div className="p-4">
+					<img src={ipsLogo} alt="IPS Logo" className="w-32 h-15 mx-auto" />
+				</div>
 
-        {/* Navegación */}
-        <nav className="flex-1 px-4 space-y-2">
-          {/* Enlace a Inicio */}
-          <a className="block py-2 px-4 rounded hover:bg-gray-700 transition-colors">
-            Inicio
-          </a>
-          {/* Enlace a Turnos */}
-          <a
-            href="#"
-            className="block py-2 px-4 rounded hover:bg-gray-700 transition-colors"
-          >
-            Turnos
-          </a>
-          {/* Botón para Nuevo Turno */}
-          <button
-            href="#"
-            className="block py-2 px-10 rounded bg-blue-500 hover:bg-blue-900 transition-colors cursor-pointer relative top-115 mx-auto"
-          >
-            Nuevo Turno
-          </button>
-        </nav>
-      </aside>
+				{/* Navegación */}
+				<nav className="flex-1 px-4 space-y-2">
+					{/* Enlace a Inicio */}
+					<a className="block py-2 px-4 rounded hover:bg-gray-700 transition-colors cursor-pointer">
+						Inicio
+					</a>
+					{/* Enlace a Turnos */}
+					<a
+						href="#"
+						className="block py-2 px-4 rounded hover:bg-gray-700 transition-colors cursor-pointer"
+					>
+						Turnos
+					</a>
+					{/* Botón para Nuevo Turno */}
+					<button
+						href="#"
+						className="block py-2 px-10 rounded bg-blue-500 hover:bg-blue-900 transition-colors cursor-pointer relative top-115 mx-auto"
+					>
+						Nuevo Turno
+					</button>
+				</nav>
+			</aside>
 
-      {/* Contenido principal */}
-      <main className="flex-1 p-8">
-        <h1>ajskasiodb</h1>
-      </main>
-    </div>
-  );
-};
+			{/* Contenido principal */}
+			<main className="flex-1 p-8">
+				<h1>ajskasiodb</h1>
+			</main>
+		</div>
+	)
+}
 // Exporta el componente para que pueda ser utilizado en otras partes de la aplicación
-export default BarraLateral;
+export default BarraLateral
