3f019bdbec3cdfc300530b589e13fdbaa8fd20b0
Albarracin-sg <camilinplin2024@gmail.com>
2025-03-07 20:47:36 -0500

    estilo formularios

--- frontend/src/components/formCard/formCard.jsx
+++ frontend/src/components/formCard/formCard.jsx
@@ -85,65 +85,119 @@ const FormCard = () => {
                     FORMULARIO DE REGISTRO
                 </h1>
 
-            {/* Contenedor de nombres */}
-                <div className="flex gap-4">
-                    <div className="w-1/2">
-                        <label htmlFor="name" className="text-[14px] font-bold">
-                            Primer Nombre
-                        </label>
-                        <input
-                            className="text-[14px] w-full p-[10px_9px] mb-[10px] border-b-[2.175px] border-b-[#6EA3C7]"
-                            type="text "
-                            name="primerNombre"
-                            value={form.primerNombre}
-                            onChange={handleChange}
-                            placeholder="Primer Nombre"
-                            required
+                    {/* Contenedor de nombres */}
+                    <div className="flex gap-4">
+                            <div className="w-1/2">
+                                <label htmlFor="name" className="text-[14px] font-bold">
+                                    Primer Nombre
+                                </label>
+                                <input
+                                    className="text-[14px] w-full p-[10px_9px] mb-[10px] border-b-[2.175px] border-b-[#6EA3C7]"
+                                    type="text "
+                                    name="primerNombre"
+                                    value={form.primerNombre}
+                                    onChange={handleChange}
+                                    placeholder="Primer Nombre"
+                                    required
+                                        />
+                            </div>
+
+                            <div className="w-1/2">
+                                <label className="text-[14px] font-bold">Segundo Nombre</label>
+                                <input 
+                                    className="text-[14px] w-full p-[10px_9px] mb-[10px] border-b-[2.175px] border-b-[#6EA3C7]"
+                                    type="text"
+                                    name="segundoNombre"
+                                    value={form.segundoNombre}
+                                    onChange={handleChange}
+                                    placeholder="Segundo Nombre"
                                 />
-                    </div>
+                            </div>
+                        </div>
 
-                    <div className="w-1/2">
-                        <label className="text-[14px] font-bold">Segundo Nombre</label>
-                        <input 
-                            className="text-[14px] w-full p-[10px_9px] mb-[10px] border-b-[2.175px] border-b-[#6EA3C7]"
-                            type="text"
-                            name="segundoNombre"
-                            value={form.segundoNombre}
-                            onChange={handleChange}
-                            placeholder="Segundo Nombre"
-                        />
-                    </div>
-                </div>
+                        {/* Contenedor de apellidos */}
+                        <div className="flex gap-4">
+                            <div className="w-1/2">
+                                <label htmlFor="lastname" className="font-bold text-[14px]">
+                                    Primer Apellido
+                                </label>
+                                <input
+                                    className="text-[14px] w-full p-[10px_9px] mb-[10px] border-b-[2.175px] border-b-[#6EA3C7]"
+                                    type="text"
+                                    name="primerApellido"
+                                    value={form.primerApellido}
+                                    onChange={handleChange}
+                                    placeholder="Primer Apellido"
+                                    required
+                                />
+                            </div>
 
-                {/* Contenedor de apellidos */}
-                <div className="flex gap-4">
-                    <div className="w-1/2">
-                        <label htmlFor="lastname" className="font-bold text-[14px]">
-                            Primer Apellido
-                        </label>
-                        <input
-                            className="text-[14px] w-full p-[10px_9px] mb-[10px] border-b-[2.175px] border-b-[#6EA3C7]"
-                            type="text"
-                            name="primerApellido"
-                            value={form.primerApellido}
-                            onChange={handleChange}
-                            placeholder="Primer Apellido"
-                            required
-                        />
-                    </div>
+                            <div className="w-1/2">
+                                <label className="text-[14px] font-bold">Segundo Apellido</label>
+                                <input 
+                                    className="text-[14px] w-full p-[10px_9px] mb-[10px] border-b-[2.175px] border-b-[#6EA3C7]" 
+                                    type="text"
+                                    name="segundoApellido"
+                                    value={form.segundoApellido}
+                                    onChange={handleChange}
+                                    placeholder="Segundo Apellido"
+                                />
+                            </div>
+                        </div>
 
-                    <div className="w-1/2">
-                        <label className="text-[14px] font-bold">Segundo Apellido</label>
-                        <input 
-                            type="text"
-                            name="segundoApellido"
-                            value={form.segundoApellido}
-                            onChange={handleChange}
-                            placeholder="Segundo Apellido"
-                            className="w-full p-2 mb-2 border-b-2 border-[#6EA3C7]" 
-                        />
-                    </div>
-                </div>
+                        {/* Contenedor de Localidad y Teléfono */}
+                        <div className="flex gap-4">
+                            {/* Localidad */}
+                            <div className="w-1/2">
+                                <label className="font-bold text-[14px]">Localidad</label>
+                                <select
+                                    name="localidad"
+                                    value={form.localidad}
+                                    onChange={handleChange}
+                                    className="text-[14px] w-full p-[10px_9px] mb-[10px] border-b-[2.175px] border-b-[#6EA3C7]"
+                                    required
+                                >
+                                    <option value="" disabled>Seleccione su localidad</option>
+                                    <option value="usaquen">Usaquén</option>
+                                    <option value="chapinero">Chapinero</option>
+                                    <option value="santa_fe">Santa Fe</option>
+                                    <option value="san_cristobal">San Cristóbal</option>
+                                    <option value="usme">Usme</option>
+                                    <option value="tunjuelito">Tunjuelito</option>
+                                    <option value="bosa">Bosa</option>
+                                    <option value="kennedy">Kennedy</option>
+                                    <option value="fontibon">Fontibón</option>
+                                    <option value="engativa">Engativá</option>
+                                    <option value="suba">Suba</option>
+                                    <option value="barrios_unidos">Barrios Unidos</option>
+                                    <option value="teusaquillo">Teusaquillo</option>
+                                    <option value="los_martires">Los Mártires</option>
+                                    <option value="antonio_nariño">Antonio Nariño</option>
+                                    <option value="puente_aranda">Puente Aranda</option>
+                                    <option value="la_candelaria">La Candelaria</option>
+                                    <option value="rafael_uribe_uribe">Rafael Uribe Uribe</option>
+                                    <option value="ciudad_bolivar">Ciudad Bolívar</option>
+                                    <option value="sumapaz">Sumapaz</option>
+                                    <option value="soacha">Soacha</option>
+                                </select>
+                            </div>
+
+                            {/* Número de Teléfono */}
+                            <div className="w-1/2">
+                                <label htmlFor="numeroTelefono" className="font-bold text-[14px]">
+                                    Número de Teléfono
+                                </label>
+                                <input
+                                    type="text"
+                                    name="numeroTelefono"
+                                    value={form.numeroTelefono}
+                                    onChange={handleChange}
+                                    placeholder="Número de Teléfono"
+                                    className="text-[14px] w-full p-[10px_9px] mb-[10px] border-b-[2.175px] border-b-[#6EA3C7]"
+                                    required
+                                />
+                            </div>
+                        </div>
 
 
                 {/* Campo para el TIPO DE DOCUMENTO */}
@@ -184,7 +238,7 @@ const FormCard = () => {
 
 
                 {/* Campo para la fecha de nacimiento */}
-                <label htmlFor="document" className="font-bold">
+                <label htmlFor="document" className="font-bold text-[14px]">
                     Fecha de Nacimiento
                 </label>
                 <input
@@ -196,50 +250,6 @@ const FormCard = () => {
                     required
                 />
 
-                {/* Localidad */}
-                <label className="font-bold text-[14px]">Localidad</label>
-                <select name="localidad"
-                value={form.localidad}
-                onChange={handleChange} 
-                className="w-full p-2 mb-2 border-b-2 border-[#6EA3C7]" 
-                required>
-
-                    <option value="" disabled>Seleccione una localidad</option>
-                    <option value="usaquen">Usaquén</option>
-                    <option value="chapinero">Chapinero</option>
-                    <option value="santa_fe">Santa Fe</option>
-                    <option value="san_cristobal">San Cristóbal</option>
-                    <option value="usme">Usme</option>
-                    <option value="tunjuelito">Tunjuelito</option>
-                    <option value="bosa">Bosa</option>
-                    <option value="kennedy">Kennedy</option>
-                    <option value="fontibon">Fontibón</option>
-                    <option value="engativa">Engativá</option>
-                    <option value="suba">Suba</option>
-                    <option value="barrios_unidos">Barrios Unidos</option>
-                    <option value="teusaquillo">Teusaquillo</option>
-                    <option value="los_martires">Los Mártires</option>
-                    <option value="antonio_nariño">Antonio Nariño</option>
-                    <option value="puente_aranda">Puente Aranda</option>
-                    <option value="la_candelaria">La Candelaria</option>
-                    <option value="rafael_uribe_uribe">Rafael Uribe Uribe</option>
-                    <option value="ciudad_bolivar">Ciudad Bolívar</option>
-                    <option value="sumapaz">Sumapaz</option>
-                    <option value="soacha">Soacha</option>
-                </select>
-
-                <label htmlFor="document" className="font-bold text-[14px]">
-                    Numero de Telefono
-                </label>
-                <input
-                    type="text"
-                    name="numeroTelefono"
-                    value={form.numeroTelefono}
-                    onChange={handleChange}
-                    placeholder="Número de Teléfono"
-                    className="text-[14px] w-full box-border p-[7.65px_9px] bg-none border-b-[2.175px] border-b-[#6EA3C7]" // Reducido en un 15%
-                    required
-                />
                 {/* Campo para el TIPO DE CITA */}
                 <label htmlFor="document" className="font-bold text-[14px]">
                     Tipo de Cita
