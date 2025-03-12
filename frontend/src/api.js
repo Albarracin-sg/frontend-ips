7bd9079fcdc861b61cbfd322d5ca43f4edc9ac3b
Albarracin-sg <camilinplin2024@gmail.com>
2025-03-11 21:42:16 -0500

    arreglo de api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,18 +1,8 @@
 import axios from 'axios';
 
-// Instancia general de la API
+// Aca va la URL de la API cuando se tenga el backend solo se cambia la url
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/Envioform"
-});
+    baseURL:"http://localhost:3000/api/Envioform"
+})
 
-
-const api2 = axios.create({
-    baseURL: "http://localhost:3000/api/api2"
-});
-
-const api3 = axios.create({
-    baseURL: "http://localhost:3000/api/api3"
-});
-
-// Exportación nombrada
-export { api, api2, api3 };
+export default api;
